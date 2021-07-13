7f5b6e5d9e857bdec47d1d1c986929f11d9caac5
mm <laythirshaid1998@gmail.com>
2021-07-13 13:41:49 +0300

    sol two assignment using react

---
+++ React/first-jsx/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hello Dogo!</h1>
+        <h2>Things I need to do:</h2>
+        <li>learn react</li>
+        <li> Climb Mt. Everest</li>
+        <li> Run a marthon</li>
+        <li> Feed the dogs</li>
+      </header>
+    </div>
+  );
+}
+
+export default App;
